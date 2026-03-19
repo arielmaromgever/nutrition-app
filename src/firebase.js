d63f25f550408d7ec7e6c62316ca550c8aeef6f1
arielmaromgever <arielmaromgever@gmail.com>
2026-03-19 19:26:54 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: ""AIzaSyDQcHrWW0lI-f8aGHxGdSa7uEwVrcY4Exg"",
+  apiKey: "AIzaSyDQcHrWW0lI-f8aGHxGdSa7uEwVrcY4Exg",
   authDomain: "nutrition-app-819ce.firebaseapp.com",
   projectId: "nutrition-app-819ce",
   storageBucket: "nutrition-app-819ce.firebasestorage.app",
@@ -15,8 +15,8 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-export const registerUser = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const loginUser = (email, password) => signInWithEmailAndPassword(auth, email, password);
+export const registerUser = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const logout = () => signOut(auth);
 export const saveUserData = (uid, data) => setDoc(doc(db, "users", uid), data, { merge: true });
 export const loadUserData = (uid) => getDoc(doc(db, "users", uid)).then(s => s.exists() ? s.data() : null);
