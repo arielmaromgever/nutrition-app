9d44ea2abc7046b4fa0f03be2dfdd7672b349ff2
arielmaromgever <arielmaromgever@gmail.com>
2026-03-19 19:00:45 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -15,7 +15,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Core Authentication and Database Functions
 export const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
 export const register = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const logout = () => signOut(auth);
